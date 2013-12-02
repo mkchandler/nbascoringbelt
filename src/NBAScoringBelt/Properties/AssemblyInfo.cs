7ff782d4e4dc453631467cac348ec96a006b2348
Matt Chandler <mkchandler@gmail.com>
2013-12-01 19:57:48 -0600

    Upped version to 1.0,1

--- src/NBAScoringBelt/Properties/AssemblyInfo.cs
+++ src/NBAScoringBelt/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@
 [assembly: AssemblyProduct("NBA Scoring Belt")]
 [assembly: AssemblyCopyright("Copyright © 2013 Matt Chandler")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
