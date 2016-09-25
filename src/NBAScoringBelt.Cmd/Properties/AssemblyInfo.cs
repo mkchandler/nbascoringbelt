8c88fb3f0446ceb41ce842250bfc6710bd1ef4c6
Matt Chandler <mkchandler@gmail.com>
2016-09-25 17:59:53 -0500

    Upped the version number

--- src/NBAScoringBelt.Cmd/Properties/AssemblyInfo.cs
+++ src/NBAScoringBelt.Cmd/Properties/AssemblyInfo.cs
@@ -1,36 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("NBAScoringBelt.Cmd")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NBAScoringBelt.Cmd")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("NBA Scoring Belt Cli")]
+[assembly: AssemblyDescription("Calculate stats for an entire season of the NBA Scoring Belt.")]
+[assembly: AssemblyProduct("NBA Scoring Belt")]
+[assembly: AssemblyCopyright("Copyright © 2016 Matt Chandler")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("701df8ac-8174-4a05-8f0b-358b551a1b49")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
--- src/NBAScoringBelt/Properties/AssemblyInfo.cs
+++ src/NBAScoringBelt/Properties/AssemblyInfo.cs
@@ -1,10 +1,10 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("NBA Scoring Belt Web Site")]
-[assembly: AssemblyDescription("Players battle throughout the season to hold the NBA Scoring Belt. ")]
+[assembly: AssemblyDescription("Players battle throughout the season to hold the NBA Scoring Belt.")]
 [assembly: AssemblyProduct("NBA Scoring Belt")]
-[assembly: AssemblyCopyright("Copyright © 2013 Matt Chandler")]
+[assembly: AssemblyCopyright("Copyright © 2016 Matt Chandler")]
 
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
-[assembly: AssemblyInformationalVersion("1.0.1")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
